cda50267cfe085d5c4d57bf09cd71f0cccbacdb4
katherinejleeming <130574058+katherinejleeming@users.noreply.github.com>
2024-06-12 20:14:43 +0100

    answers to reduxPart2 added

--- Fullstack_Paper_1/reduxPart2.js
+++ Fullstack_Paper_1/reduxPart2.js
@@ -1,23 +1,10 @@
-import React, { useState } from 'react';
-
 const classInfo = () => {
     let [studentsCount, setStudentsCount] = useState(0);
-    
-    const students = [
-        {name: "Nrupul", present: false},
-        {name: "Prateek", present: true},
-    ];
-
-    
-    const addStudent = () => {
-        
-    
-    }
 
     return(
         <div>
             <p>Number of students in class room: {studentsCount}</p>
-            <button onClick={addStudent}>Add Student</button>
+            <button onClick={setStudentsCount}>Add Student</button>
         </div>
     )
 };
@@ -25,7 +12,24 @@ const classInfo = () => {
 
 
 // 1. Can you provide a brief summary on what is happening on line 34, 39?
+
 // Line 34 is setting the states of studentsCount and setStudentsCount to 0
 // Line 39 is a button to add a student to the studentCount but it missing the function to add a student
 
-//2.
+// When a user clicks on the “Add student” button update the state (studentsCount) to
+// include only the total number of students who are present.
+// a. Write a pseudocode of how your function would look.
+
+// INPUT {name: x , present = bool}
+// IF present = true THEN
+// UPDATE state.studentsCount + 1
+// RETURN alert('Student Added')
+// ELSE Break
+
+// b. How do you ensure that the function is triggered when the button is
+// clicked?
+
+// Simply, you could add a console.log/ alert to mark if the function has triggered
+
+// c. How will you update the state with the result of your function?
+//  using dispatch(action)
--- Fullstack_Paper_1/reduxPart3.js
+++ Fullstack_Paper_1/reduxPart3.js
@@ -13,9 +13,19 @@ function countReducer(state = initialState, action){
 };
 
 // 1. A change of code was made on line 174 (figure 4), can you briefly explain what that would do?
+
 // 
 
 // 2. Let’s say we don’t want to set the state locally anymore and want to use dispatch.
 // How would you ensure that an “increment” action that also contains the result of the
 // studentCount is dispatched on button click? According to your answer in part 2.2b
 // what would need to be changed?
+
+//
+
+
+// Which code do you think is best suited to ensure that the “increment” action updates
+// the state with the correct total number of students who are present. Is it Figure 4? Or
+// Figure 5? Explain the code difference and your reasoning
+
+// 
